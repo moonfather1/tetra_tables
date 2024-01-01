3338053856bff477896fbd21e3ab5e384bb8ae1f
MoonFather <moonfather@homemail.com>
2024-01-01 20:13:41 +0100

    aligned block list with workshop

--- src/main/java/moonfather/tetra_tables/initialization/DynamicRegistration.java
+++ src/main/java/moonfather/tetra_tables/initialization/DynamicRegistration.java
@@ -1,9 +1,12 @@
 package moonfather.tetra_tables.initialization;
 
+import moonfather.tetra_tables.Constants;
 import moonfather.tetra_tables.blocks.TetraTable;
 import moonfather.tetra_tables.blocks.WoodenBlockItem;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.EntityTypeTags;
+import net.minecraft.world.entity.animal.Sheep;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.fml.ModList;
@@ -36,23 +39,17 @@ public class DynamicRegistration
             boolean frozen = ((ForgeRegistry) ForgeRegistries.BLOCKS).isLocked();
             if (frozen) { ((ForgeRegistry) ForgeRegistries.BLOCKS).unfreeze(); }
 
-            List<ResourceLocation> registeredBlocks = ForgeRegistries.BLOCKS.getKeys().stream().toList(); // because we'll be adding to collection
-            for (ResourceLocation id: registeredBlocks)
+            for (String wood : DynamicWoodListAccessor.getWoodIds())
             {
-                if (! id.getNamespace().equals(mc) && id.getPath().endsWith(planks) && ! id.getPath().contains(vertical))
+                if (DynamicWoodListAccessor.getHostMod(wood).equals(mc) && ForgeRegistries.BLOCKS.containsKey(new ResourceLocation(Constants.MODID, "tetra_table_" + wood)))
                 {
-                    // looks like wood so far. let's check for slabs and logs as we need them for recipes
-                    String wood = id.getPath().replace(planks, "");
-                    if (ForgeRegistries.BLOCKS.containsKey(new ResourceLocation(id.getNamespace(), id.getPath().replace(planks, slab)))
-                            && ForgeRegistries.BLOCKS.containsKey(new ResourceLocation(id.getNamespace(), LOG1 + wood + LOG2)))
-                    {
-                        Block block = new TetraTable();
-                        Item item = new WoodenBlockItem(block);
-                        ForgeRegistries.BLOCKS.register("tetra_table_" + wood, block);
-                        ForgeRegistries.ITEMS.register("tetra_table_" + wood, item);
-                        Registration.blocks_table3.add(() -> block);
-                    }
+                    return; // done manually, acacia for example
                 }
+                Block block = new TetraTable();
+                Item item = new WoodenBlockItem(block);
+                ForgeRegistries.BLOCKS.register("tetra_table_" + wood, block);
+                ForgeRegistries.ITEMS.register("tetra_table_" + wood, item);
+                Registration.blocks_table3.add(() -> block);
             }
             if (frozen) { ((ForgeRegistry) ForgeRegistries.BLOCKS).freeze(); }
         }
