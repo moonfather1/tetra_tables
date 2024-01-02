debda8c7b85b0cda0b7be4ad9746c2884c3e68e7
MoonFather <moonfather@homemail.com>
2024-01-02 14:03:15 +0100

    added advancement and fixed old issues 1.20

--- src/main/java/moonfather/tetra_tables/HammerEvent.java
+++ src/main/java/moonfather/tetra_tables/HammerEvent.java
@@ -1,13 +1,17 @@
 package moonfather.tetra_tables;
 
+import net.minecraft.advancements.Advancement;
+import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.Event;
@@ -17,36 +21,46 @@ import se.mickelus.tetra.TetraToolActions;
 
 public class HammerEvent
 {
+    private static final ResourceLocation WORKBENCH_ADVANCEMENT = new ResourceLocation("tetra", "upgrades/workbench");
+
     public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event)
     {
         BlockState state = event. getLevel().getBlockState(event.getPos());
         if (state.is(workbench) && ! event.getEntity().isCrouching())
         {
-            if (event.getHand().equals(InteractionHand.MAIN_HAND) && event.getEntity().getMainHandItem().canPerformAction(TetraToolActions.hammer))
+            if (! state.is(Blocks.CRAFTING_TABLE))  // leave that one alone
             {
-                Block newTable = tryGetTable(state.getBlock());
-                if (newTable != null)
+                if (event.getHand().equals(InteractionHand.MAIN_HAND) && event.getEntity().getMainHandItem().canPerformAction(TetraToolActions.hammer))
                 {
-                    event.getLevel().setBlockAndUpdate(event.getPos(), newTable.defaultBlockState());
-                    event.getLevel().playSound(event.getEntity(), event.getPos(), SoundEvents.WOOD_PLACE, SoundSource.BLOCKS, 1.0F, 0.5F);
-                }
-                else
-                {
-                    if (event.getLevel().isClientSide)
+                    Block newTable = tryGetTable(state.getBlock());
+                    if (newTable != null)
                     {
-                        if (ModList.get().isLoaded("everycomp"))
+                        event.getLevel().setBlockAndUpdate(event.getPos(), newTable.defaultBlockState());
+                        event.getLevel().playSound(event.getEntity(), event.getPos(), SoundEvents.WOOD_PLACE, SoundSource.BLOCKS, 1.0F, 0.5F);
+                        if (event.getEntity() instanceof ServerPlayer serverPlayer)
                         {
-                            event.getEntity().displayClientMessage(tableNotFound2, true);
-                        }
-                        else
-                        {
-                            event.getEntity().displayClientMessage(tableNotFound1, true);
+                            Advancement a = serverPlayer.getServer().getAdvancements().getAdvancement(WORKBENCH_ADVANCEMENT);
+                            serverPlayer.getAdvancements().award(a, "hammer");
                         }
                     }
+                    else
+                    {
+                        if (event.getLevel().isClientSide)
+                        {
+                            if (ModList.get().isLoaded("workshop_for_handsome_adventurer"))
+                            {
+                                event.getEntity().displayClientMessage(tableNotFound2, true);
+                            }
+                            else
+                            {
+                                event.getEntity().displayClientMessage(tableNotFound1, true);
+                            }
+                        }
+                    }
+                    event.setResult(Event.Result.DENY);
+                    event.setUseItem(Event.Result.DENY);
+                    event.setUseBlock(Event.Result.DENY);
                 }
-                event.setResult(Event.Result.DENY);
-                event.setUseItem(Event.Result.DENY);
-                event.setUseBlock(Event.Result.DENY);
             }
         }
     }
@@ -63,7 +77,7 @@ public class HammerEvent
         {
             wood = id.substring(namePos + 13);
         }
-        else
+        if (wood == null)
         {
             namePos = id.indexOf("_crafting_table");
             if (namePos >= 0)
@@ -73,16 +87,28 @@ public class HammerEvent
         }
         if (wood == null)
         {
+            if (id.startsWith("crafting_table_"))
+            {
+                wood = id.substring(15);
+            }
+        }
+        if (wood == null)
+        {
             return null;
         }
 
-        String newName = "tetra_table_" + wood;
-        for (ResourceLocation key: ForgeRegistries.BLOCKS.getKeys())
+//        String newName = "tetra_table_" + wood;
+//        for (ResourceLocation key: ForgeRegistries.BLOCKS.getKeys())
+//        {
+//            if (key.toString().endsWith(newName))
+//            {
+//                return ForgeRegistries.BLOCKS.getValue(key);
+//            }
+//        }             //EveryCompat
+        ResourceLocation rl = new ResourceLocation(Constants.MODID, "tetra_table_" + wood);
+        if (ForgeRegistries.BLOCKS.containsKey(rl))
         {
-            if (key.toString().endsWith(newName))
-            {
-                return ForgeRegistries.BLOCKS.getValue(key);
-            }
+            return ForgeRegistries.BLOCKS.getValue(rl);
         }
         return null;
     }
